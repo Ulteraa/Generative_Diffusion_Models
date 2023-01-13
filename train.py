7378d10b5101c1272b97f1995a35ccd1389c777d
Fariborz Taherkhani <ft0009@mix.wvu.edu>
2023-01-12 20:59:10 -0500

    Update train.py

--- train.py
+++ train.py
@@ -50,7 +50,8 @@ class Diffusion:
                 x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
         model.train()
         x = (x.clamp(-1, 1) + 1) / 2
-        x = (x * 255).type(torch.uint8)
+        # x = (x * 255).type(torch.uint8)
+        x = (x * 255)
         return x
 
 def data_loder():
